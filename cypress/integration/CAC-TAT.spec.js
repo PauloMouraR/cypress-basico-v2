5c50ede57221dc92d151268b9d66982da0286788
Paulo Moura <paulomourocha@gmail.com>
2024-05-15 20:15:24 -0300

    Alguns outros exercicios

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -162,4 +162,34 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           .click()
           cy.contains('Talking About Testing').should('be.visible')
     })
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function(){
+        cy.get('.success').should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Mensagem enviada com sucesso.')
+            .invoke('hide')
+            .should('not.be.visible')
+        cy.get('.error').should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Valide os campos obrigatórios!')
+            .invoke('hide')
+            .should('not.be.visible')
+    })
+    it('preenche a area de texto usando o comando invoke', function(){
+        const textoLongo = Cypress._.repeat('ABCDEFGHIJ', 20)
+
+        cy.get('#open-text-area')
+          .invoke('val', textoLongo)
+          .should('have.value', textoLongo)
+    })
+    it('faz uma requisição HTTP', function(){
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+          .should(function(response){
+            const {status, statusText, body} = response
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+        })
+    })
 })
---
+++ cypress/integration/privacy.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+Cypress._.times(5, function(){
+    it('testa a pagina da politica de privacidade de forma independente', function(){
+        cy.visit('./src/privacy.html')
+
+        cy.contains('Talking About Testing').should('be.visible')
+    })
+})
